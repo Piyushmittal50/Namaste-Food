04d90c4a9c4f7c212a3fbf46330bf8619c3e6ff8
piyushmittal50 <mittalpiyush87483@gmail.com>
2023-12-31 15:15:48 +0530

    React

--- App.js
+++ App.js
@@ -1,33 +1,5 @@
-//const heading =  React.createElement("h1",{},"hello world from React!");
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(heading);
-   
-
-/*
-
-<div id="parent">
-  <div id="child">
-    <h1>I am h1 Tag</h1>
-  </div>
-</div>
-
-*/
-
-//const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "I am a h1 Tag")));
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(parent);
-
-/*
-
-<div id="parent">
-  <div id="child">
-    <h1>I am h1 Tag</h1>
-    <h2>I am h2 Tag</h2>
-  </div>
-</div>
-
-*/
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" },
     [React.createElement("h1", {}, "I am a h1 Tag"),React.createElement("h2", {}, "I am a h2 Tag")]
 )
---
+++ README.md
@@ -0,0 +1,12 @@
+# React
+
+# Parcel
+- Dev Build
+- Provide Local Sever
+- HMR = hot module replacement
+- File Watching Algo - written in c++
+- Catching = Faster Builds 
+- Image optimization
+- Bundler
+- Compress File 
+- Error Handling 
