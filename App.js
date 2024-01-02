e389a1b93de9e51bb4e45741fd6dc2fab71d77bc
piyushmittal50 <mittalpiyush87483@gmail.com>
2024-01-02 13:06:23 +0530

    React

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" },
-    [React.createElement("h1", {}, "I am a h1 Tag"),React.createElement("h2", {}, "I am a h2 Tag")]
-)
+
+//const heading = React.createElement("h1", { id: "heading" }, "Nameste React by AK 🚀"); // older way to create react Element
+const heading = (<h1 id="heading">React by Akshay saini 🚀</h1>);  // It is React Element , and easy to write
+
+const Title = () => (
+    <h1 id="heading">
+        Nameste JavaScript
+    </h1>
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// React functional component
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />    {/*component composition  */}
+        {heading}  {/*rendering a react Element inside a component  */}
+    <h1 id="heading">Nameste React Functional component</h1>
+    </div>
+);
+const dom = ReactDOM.createRoot(document.getElementById("root"));
+dom.render(<HeadingComponent />);
+//dom.render(hedaing);
+
+
